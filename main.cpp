c038bfe5afbf2089f062dad2c645d8fea7e636ee
Шкуратов Алексей Геннадьевич <ip912s28@awp06.csc.local>
2020-02-14 12:44:57 +0700

    format added

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
-  cout<<"Hello world"<<endl;
-  cout<<"Hello my friend"<<endl;
+    cout << "Hello world" << endl;
+    cout << "Hello my friend" << endl;
     return 0;
 }
